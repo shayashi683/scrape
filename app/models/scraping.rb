4a639362d97d4bfb009e91b7b7adb11b8ab09909
shayashi683 <shayashi683@gmail.com>
2020-08-02 20:30:16 +0900

    SCRAPING

---
+++ app/models/scraping.rb
@@ -0,0 +1,24 @@
+require 'mechanize'
+class Scraping < ApplicationRecord
+    def self.fuga_urls
+    agent = Mechanize.new
+    links = []
+    next_url = ""
+
+    while true
+      current_page = agent.get("https://researchmap.jp/read0170729/published_papers?limit=20&offset=1" + next_url)
+      elements = current_page.search('li a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('//*[@id="frame-19159"]/div/div[2]/nav/nav/ul/li[6]/a')
+      p next_link
+      break unless next_link
+      next_url = next_link.get_attribute('href')
+      p "aaa"
+      p next_url
+    end
+    p elements.inner_text
+  end
+end
