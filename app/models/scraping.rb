41b461aec66af9e1d63749795850c3442f474c3c
shayashi683 <shayashi683@gmail.com>
2020-08-08 00:37:21 +0900

    あああ
    aaa

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,43 +1,22 @@
 require 'mechanize'
 class Scraping < ApplicationRecord
     def self.fuga_urls
-        # mechanizeを開始
-​
-        # Mechanize定義
-        agent = Mechanize.new
-​
-        # ドメイン名定義
-        domain = "https://researchmap.jp"
-        next_url = "/read0170729/published_papers?limit=20&offset=1" #1ページ目
-​
-        # 無限ループしてしまうと相手のサイトに迷惑をかけてしまうのでまずは次のページへのリンクを取得できるか
-        # だけ確認するために、whileはコメントアウト
-        #while true
-            # ページを取得する
-            current_page = agent.get(domain + next_url)
-​
-            # activeクラスがあったらフラグを立てる
-            active_flag = 0
-​
-            # li内のリンクを全て取得する
-            elements = current_page.search('li')
-            elements.each do |element|
-                # もしactiveフラグがオンだったらリンクを取得する(activeクラスを持った次のliは次へのリンクなはず)
-                if active_flag == 1
-                    next_link = element.get_attribute('href')
-                    break
-                end
-​
-                # liにactiveクラスがついていたらフラグを立てる(element.classが動くか微妙...)
-                if element.class == "active"
-                    active_flag = 1
-                end
-            end
-​
-            # 「/read0170729/published_papers?limit=20&offset=21」と表示させたい
-            p next_link
-        #end
-​
-        # mechanize終了
+    agent = Mechanize.new
+    links = []
+    next_url = ""
+    while true
+      current_page = agent.get("https://researchmap.jp/read0170729/published_papers?limit=20&offset=1" + next_url)
+      elements = current_page.search('li a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+      next_link = current_page.at('//*[@id="frame-19159"]/div/div[2]/nav/nav/ul/li[6]/a')
+      p next_link
+      break unless next_link
+      next_url = next_link.get_attribute('href')
+      p "aaa"
+      p next_url
     end
+    p elements.inner_text
+  end
 end
