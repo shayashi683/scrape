6b8353241271cd8511f8b2dc7bb43afbe353f797
shayashi683 <63200753+shayashi683@users.noreply.github.com>
2020-08-09 14:03:51 +0900

    update scraping file

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -8,3 +8,27 @@ class Scraping < ApplicationRecord
     page_count = 20
     next_url = "/read0170729/published_papers?limit=20&offset=#{default_page}"
     next_link = ""
+
+    # while true do
+      current_page = agent.get(domain + next_url)
+      # activeクラスがあったらフラグを立てる
+      active_flag = 0
+      # li内のリンクを全て取得する
+      elements = current_page.search('.pagination li')
+      elements.each do |element|
+          # もしactiveフラグがオンだったらリンクを取得する(activeクラスを持った次のliは次へのリンクなはず)
+          if active_flag == 1
+            next_link = element.at("a")["href"]
+            break
+          end
+          # liにactiveクラスがついていたらフラグを立てる
+          if element.first.present? && element.first[1] == "active"
+            active_flag = 1
+          end
+      end
+      default_page = default_page + page_count
+      # 「/read0170729/published_papers?limit=20&offset=21」と表示させたい
+      p next_link
+    # ​end
+  end
+end
