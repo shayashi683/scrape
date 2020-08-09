ad2eb73b269571d937b1a23d1fa60cd27a4d4645
shayashi683 <shayashi683@gmail.com>
2020-08-10 00:56:11 +0900

    get next_link

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -8,27 +8,37 @@ class Scraping < ApplicationRecord
     page_count = 20
     next_url = "/read0170729/published_papers?limit=20&offset=#{default_page}"
     next_link = ""
-
-    # while true do
+    pre_next_link = ""
+    active_flag = 1
+    page_num = 1
+    while active_flag == 1 do
       current_page = agent.get(domain + next_url)
       # activeクラスがあったらフラグを立てる
       active_flag = 0
       # li内のリンクを全て取得する
       elements = current_page.search('.pagination li')
-      elements.each do |element|
-          # もしactiveフラグがオンだったらリンクを取得する(activeクラスを持った次のliは次へのリンクなはず)
-          if active_flag == 1
-            next_link = element.at("a")["href"]
-            break
+      elements.each_with_index do |element, i|
+        # もしactiveフラグがオンだったらリンクを取得する(activeクラスを持った次のliは次へのリンクなはず)
+        if active_flag == 1
+          next_link = element.at("a")["href"]
+          break
+        end
+        # liにactiveクラスがついていたらフラグを立てる
+        if element.first.present? && element.first[1] == "active"
+          active_flag = 1
+          # 前回のループと同じリンクだった場合、while文を抜けるためにactive_flagをオフにする
+          if i != 0 && pre_next_link == next_link
+            active_flag = 0
+          else
+            # ここに対象のページでやりたい処理を書いていく
+            p "#{next_url}"
           end
-          # liにactiveクラスがついていたらフラグを立てる
-          if element.first.present? && element.first[1] == "active"
-            active_flag = 1
-          end
+          pre_next_link = next_link
+        end
       end
       default_page = default_page + page_count
-      # 「/read0170729/published_papers?limit=20&offset=21」と表示させたい
-      p next_link
-    # ​end
+      next_url = "/read0170729/published_papers?limit=20&offset=#{default_page}"
+      page_num = page_num + 1
+    end
   end
 end
